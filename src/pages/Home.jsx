82913dca179a52d2b550b883facf7198c17e6738
TotiFlame <santiagoferna4321@gmail.com>
2023-10-15 14:54:17 -0300

    Button to dowload cv made

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,14 @@
 function Home() {
     return (
-        <section id="home-section" className="w-full h-full flex justify-center items-center">
-            <h1 className="titles-font text-white">SANTIAGO FERNANDEZ</h1>
+        <section id="home-section" className="w-full h-full flex flex-col justify-center items-center">
+            <h1 className="titles-font text-white mb-8">SANTIAGO FERNANDEZ</h1>
+            <a
+                href="/my-CV.pdf"
+                download="Santiago Fernandez CV"
+                className="text-white p-4 bg-blue-500 rounded-xl"
+            >
+                    Download my CV
+            </a>
         </section>
     )
 }
