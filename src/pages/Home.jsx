ea748d6086b65e37676702edae6c96e2a6c205cb
TotiFlame <santiagoferna4321@gmail.com>
2023-10-20 17:28:46 -0300

    Framer motion installed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,27 @@
+'use client'
+import { color, motion } from "framer-motion"
+
 function Home() {
     return (
         <section id="home-section" className="w-full h-full flex flex-col justify-center items-center">
-            <h1 className="titles-font text-white mb-8">SANTIAGO FERNANDEZ 👨🏽‍💻</h1>
-            <a
+            <motion.h1
+                initial={{ opacity: 0}}
+                transition={{ duration: 3}}
+                animate={{ opacity: 1}}
+                className="titles-font text-white mb-8"
+            >
+                SANTIAGO FERNANDEZ 👨🏽‍💻
+            </motion.h1>
+            <motion.a
                 href="/my-CV.pdf"
                 download="Santiago-Fernandez-CV"
+                initial={{ opacity: 0}}
+                transition={{ duration: 3}}
+                animate={{ opacity: 1}}
                 className="text-white p-4 bg-blue-500 rounded-xl"
             >
                     Download my CV
-            </a>
+            </motion.a>
         </section>
     )
 }
