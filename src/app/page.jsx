9afeceace1c03f78406cb0d2eb16febe47d78382
TotiFlame <santiagoferna4321@gmail.com>
2023-11-05 12:46:17 -0300

    Header hided

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,30 +1,31 @@
 "use client"
 import Header from "@/components/Header";
-import PhoneHeader from "@/components/PhoneHeader";
+// import PhoneHeader from "@/components/PhoneHeader";
 import Footer from "@/components/Footer";
 import Home from "@/pages/Home";
 import AboutMe from "@/pages/AboutMe";
 import Projects from "@/pages/Projects";
-import { useState } from "react";
-import { FiMenu } from 'react-icons/fi';
+// import { useState } from "react";
+// import { FiMenu } from 'react-icons/fi';
 
 
 export default function () {
-    const [openHeader, setOpenHeader] = useState(false);
+    // const [openHeader, setOpenHeader] = useState(false);
 
-    const handleOpenHeader = () => {
-        setOpenHeader(!openHeader);
-    }
+    // const handleOpenHeader = () => {
+    //     setOpenHeader(!openHeader);
+    // }
 
     return (
         <>
-            <FiMenu
+            {/* <FiMenu
                 className="absolute z-50 invisible text-white text-4xl ml-2 mt-2 phone:visible"
                 onClick={handleOpenHeader}
-            />
-            {
-                openHeader ? <PhoneHeader/> : <Header/>
-            }
+                />
+                {
+                    openHeader ? <PhoneHeader/> : <Header/>
+                } */}
+            <Header />
             <Home />
             <AboutMe />
             <Projects />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@
 
 function Header () {
     return (
-        <header className="fixed z-30 top-0 w-full h-12 flex justify-center phone:items-center phone:justify-end">
+        <header className="fixed z-30 top-0 w-full h-12 flex justify-center phone:invisible">
             <ul className="w-2/3 h-full flex justify-evenly items-center uppercase phone:invisible">
                 <li className="header-links">
                     <a href="#home-section">[Home]</a>
